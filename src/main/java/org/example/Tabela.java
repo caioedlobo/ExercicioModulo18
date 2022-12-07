e5e000cd5ca2f813a8a34ec96e48d76e91a3eead
Caio Lobo <caioeduardolobo@gmail.com>
2022-12-07 10:51:13 -0300

    remove ElemntType.FIELD

--- src/main/java/org/example/Tabela.java
+++ src/main/java/org/example/Tabela.java
@@ -4,7 +4,7 @@ import java.lang.annotation.*;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD, ElementType.TYPE})
+@Target({ElementType.TYPE})
 public @interface Tabela {
     String value();
 }

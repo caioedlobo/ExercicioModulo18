2ec32a1d13ea536c3542f10adaddafa51ae493e6
Caio Lobo <caioeduardolobo@gmail.com>
2022-12-07 10:49:44 -0300

    first commit

---
+++ src/main/java/org/example/ClasseComAnnotation.java
@@ -0,0 +1,7 @@
+package org.example;
+
+@Tabela(value = "Tabela de Pessoas")
+public class ClasseComAnnotation {
+    private String nome;
+    private String codigo;
+}
---
+++ src/main/java/org/example/Tabela.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.TYPE})
+public @interface Tabela {
+    String value();
+}
